4f3c80fb6436ce25b835407a85d624a9c7d526c2
Hasan Karadiya <hasankaradiya1626@gmail.com>
2024-07-31 22:46:10 +0530

    Setup Admin Login

---
+++ resources/views/admin/auth/login.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Dalima World | Login Page</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  {{-- Favicons --}}
+  {{-- <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> --}}
+
+  {{-- Google Fonts --}}
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  {{-- Vendor CSS Files --}}
+  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+
+  {{-- Template Main CSS File --}}
+  <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                  <span class="d-none d-lg-block">Dalima World</span>
+                </a>
+              </div>
+
+              <div class="card mb-3">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                    <p class="text-center small">Enter your username & password to login</p>
+                  </div>
+
+                  <form class="row g-3">
+
+                    <div class="col-12">
+                      <label for="email" class="form-label">Email</label>
+                      <div class="input-group">
+                          <input type="text" name="email" class="form-control" id="email" placeholder="Enter Your Email">
+                          <span class="input-group-text"><i class="bi bi-envelope"></i></span>
+                      </div>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="yourPassword" class="form-label">Password</label>
+                      <div class="input-group">
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Enter Your Password">
+                            <span class="input-group-text"><i class="bi bi-eye"></i></span>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                      <button class="btn btn-primary w-100" type="submit">Login</button>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+    </div>
+  </main>
+
+</body>
+
+</html>
